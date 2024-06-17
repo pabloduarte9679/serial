2bb4ac0161ef9d1625d9b2b005e4fe55f54b9df9
pablo <92185570+pablo756xD@users.noreply.github.com>
2024-06-17 17:19:58 -0600

    fixed bugs

--- serial.c
+++ serial.c
@@ -6,42 +6,38 @@
 #include <string.h>
 #include <stdint.h>
 
-int main(){
+int main() {
+    int serialPort = open("/dev/ttyUSB0", O_RDWR);
 
-  int serialPort = open("/dev/ttyUSB0", O_RDWR);
+    if (serialPort < 0) {
+        perror("unable to open serial port");
+        exit(1);
+    }
 
-  if(serialPort < 0){
-    perror("enable to open serial port");
-    exit(1);
-  }
+    struct termios tty;
+    memset(&tty, 0, sizeof(tty));
 
-  struct termios tty;
-  memset(&tty,0,sizeof(tty));
+    if (tcgetattr(serialPort, &tty) != 0) {
+        perror("unable to get serial port attr");
+        close(serialPort);
+        exit(1);
+    }
 
-  if(tcgetattr(serialPort, &tty) != 0){
-    perror("unable to get serial port attr");
+    cfsetospeed(&tty, B9600);
+    cfsetispeed(&tty, B9600);
+
+    tty.c_cflag &= ~PARENB; 
+    tty.c_cflag &= ~CSTOPB; 
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8; 
+
+    tcsetattr(serialPort, TCSANOW, &tty);
+
+    char hex = 0x79;
+    while (1) {
+        write(serialPort, &hex, sizeof(char));
+    }
+
     close(serialPort);
-    exit(1);
-  }
-
-  cfsetospeed(&tty,B9600);
-  cfsetispeed(&tty,B9600);
-
-  tty.c_cflag |= (CLOCAL | CREAD);
-  tty.c_cflag &= ~PARENB;
-  tty.c_cflag &= ~CSTOPB;
-  tty.c_cflag &= ~CSIZE;
-  tty.c_cflag |= CS8;
-  tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); 
-  tty.c_iflag &= ~(IXON | IXOFF | IXANY); 
-  tty.c_oflag &= ~OPOST; 
-  tcsetattr(serialPort, TCSANOW, &tty);
-  char letter = 'A';
-	while(1){
-  write(serialPort, &letter, sizeof(char));
-  }
-
-  close(serialPort);
-  return 0;
-
+    return 0;
 }
